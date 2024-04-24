2ee3dcc494ea5ceac5b105276cc6d02e2d65198b
Anton Ovod <antovod36@gmail.com>
2024-04-24 22:01:57 +0200

    Model for username has been implemented

---
+++ GreetingApp/Models/UserNameModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreetingApp.Models
+{
+    public class UserNameModel
+    {
+        [Display(Name = "Enter your name: ")]
+        [Required(ErrorMessage="Please enter your name")]
+        [RegularExpression("^[A-Z]([a-z]{2,})", 
+            ErrorMessage ="Please enter a valid name")]
+
+        public string UserName { get; set; }
+        public UserNameModel()
+        {
+            UserName = "";
+        }
+
+        public UserNameModel(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
